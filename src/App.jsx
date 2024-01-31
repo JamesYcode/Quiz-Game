97e48f50ba15afda917167ea9e484ed58544575d
James <909sunmin@gmail.com>
2024-01-30 22:44:16 -0500

    added main page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import Header from './components/Header';
+import Selections from './components/Selections';
+
 function App() {
-  return <div className='app'></div>;
+  return (
+    <div className='app'>
+      <Header />
+      <Selections />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Selections.jsx
@@ -0,0 +1,12 @@
+function Selections() {
+  return (
+    <div className='selection-container'>
+      <h3>HTML</h3>
+      <h3>CSS</h3>
+      <h3>JavaScript</h3>
+      <h3>React.js</h3>
+    </div>
+  );
+}
+
+export default Selections;
